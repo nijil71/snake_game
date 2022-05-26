df64ced11fa163ec64ee46f61bfc2a5d4e2d5324
nijil <nmnijilkhan@gmail.com>
2022-05-26 12:39:14 +0530

    added highest score

--- snake_game/score_board.py
+++ snake_game/score_board.py
@@ -1,24 +1,33 @@
 from turtle import Turtle
 
+
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score=0
+        self.score = 0
+        with open('data.txt') as data:
+            self.highest_score = int(data.read())
+
         self.color('white')
         self.penup()
-        self.goto(0,270)
+        self.goto(0, 270)
         self.hideturtle()
         self.update_score()
 
     def update_score(self):
-        self.write(f'Score:{self.score}',align='center', font=('Courier', 20, 'normal'))
-
-    def game_over(self):
-        self.goto(0,0)
-        self.write('Game Over.', align='center', font=('Courier', 20, 'normal'))
-
-    def increase_score(self):
-        self.score+=1
         self.clear()
+        self.write(f'Score:{self.score} Highest score:{self.highest_score}', align='center',
+                   font=('Courier', 20, 'normal'))
+
+    def reset(self):
+        if self.score > self.highest_score:
+            self.highest_score = self.score
+            with open('data.txt', mode='w') as data:
+                data.write(f'{self.highest_score}')
+        self.score = 0
         self.update_score()
 
+    def increase_score(self):
+        self.score += 1
+
+        self.update_score()
